bf6dcdbac35783a16f6c8f42d6ff4a084e4242d8
fk2000 <fujiwara.kenta@elevennines.co.jp>
2017-12-06 12:48:55 +0000

    form add

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var items = [
+    { "text": "1st Post." }
+    , { "text": "2nd Post." }
+];
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Entry List',items: items });
+});
+
+
+router.get('/form', function(req, res) {
+    res.render('form', { title: 'New Entry' })
+});
+
+router.post('/create', function(req, res){
+    console.log(req.body.text);
+    res.redirect('/');
 });
 
 module.exports = router;
