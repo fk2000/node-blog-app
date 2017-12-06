7a96eb9a8778bf014c1efd52f089946817ceaaf8
fk2000 <fujiwarak@gmail.com>
2017-12-06 16:41:01 +0900

    first commit

---
+++ routes/main.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+var items = [
+    { "text": "1st Post." }
+    , { "text": "2nd Post." }
+];
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Entry List',items: items });
+});
+
+
+router.get('/form', function(req, res) {
+    res.render('form', { title: 'New Entry' })
+});
+
+router.post('/create', function(req, res){
+    console.log(req.body.text);
+    res.redirect('/');
+});
+
+module.exports = router;
